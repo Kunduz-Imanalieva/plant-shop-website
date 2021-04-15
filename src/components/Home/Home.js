c4c5db541b44816bcaeb3616cadc9091a151109d
Kunduz-Imanalieva <imakuzya@gmail.com>
2021-04-15 22:19:14 +0600

    added home's background image

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,9 @@
+import classes from "./Home.module.css";
+import homeBackground from "../../images/home.svg"
+
 const Home = () => {
-    return ( <div>
-    this is homepage
+    return ( <div className={classes.Home}>
+        <img src={homeBackground} alt="background" />
     </div> );
 }
  
