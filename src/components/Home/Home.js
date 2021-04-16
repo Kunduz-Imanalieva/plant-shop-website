ff0169f0ffcc2034b4021d271c9168adbda2e48f
Kunduz-Imanalieva <imakuzya@gmail.com>
2021-04-16 09:35:16 +0600

    gave some styles to App.css

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,14 @@
 import classes from "./Home.module.css";
-import homeBackground from "../../images/home.svg"
+import homeBackground from "../../images/home.svg";
 
 const Home = () => {
-    return ( <div className={classes.Home}>
-        <img src={homeBackground} alt="background" />
-    </div> );
-}
- 
+  return (
+    <div className={classes.Home}>
+      <div>
+        <img src={homeBackground} alt="background" width="1311px" />
+      </div>
+    </div>
+  );
+};
+
 export default Home;
