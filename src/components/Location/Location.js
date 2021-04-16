431304706d14de784f2fe8eab0d588b77d964a4b
Kunduz-Imanalieva <imakuzya@gmail.com>
2021-04-16 10:19:05 +0600

    added an image to location

--- src/components/Location/Location.js
+++ src/components/Location/Location.js
@@ -1,7 +1,14 @@
+import locationBackground from "../../images/location.svg";
+
 const Location = () => {
-    return ( <div>
-        This is Location
-    </div> );
-}
- 
+  return (
+    <div>
+      <div>
+        <img src={locationBackground} alt="location" width="1311px" />
+      </div>
+      
+    </div>
+  );
+};
+
 export default Location;
