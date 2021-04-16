76b61a204fe8b7f811ba10aefd79b51bf4ddb01c
Kunduz-Imanalieva <imakuzya@gmail.com>
2021-04-16 11:34:39 +0600

    added h1 and p and stylized them

--- src/components/Service/Service.js
+++ src/components/Service/Service.js
@@ -1,12 +1,18 @@
+import classes from "./Service.module.css";
 import serviceBackground from "../../images/service.svg";
 
 const Service = () => {
-    return ( <div>
-        <div>
-            <img src={serviceBackground} alt="service" width="1311px"/>
-            
+  return (
+    <div>
+      <div>
+        <img src={serviceBackground} alt="service" width="1311px" />
+        <div className={classes.Service}>
+          <h1>Service for home plants.</h1>
+          <p>If you don’t know what plants you can add to the space,we can provide you with selected plants,and configure your space.</p>
         </div>
-    </div> );
-}
- 
+      </div>
+    </div>
+  );
+};
+
 export default Service;
