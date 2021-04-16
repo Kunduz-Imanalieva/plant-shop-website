21332265ee3fd1be1765a6842be781b6d42c212c
Kunduz-Imanalieva <imakuzya@gmail.com>
2021-04-16 11:03:54 +0600

    filled with content and stylized them

--- src/components/Location/Location.js
+++ src/components/Location/Location.js
@@ -1,3 +1,4 @@
+import classes from "./Location.module.css";
 import locationBackground from "../../images/location.svg";
 
 const Location = () => {
@@ -5,7 +6,16 @@ const Location = () => {
     <div>
       <div>
         <img src={locationBackground} alt="location" width="1311px" />
+         <div className={classes.Location}>
+             <h1>Location</h1>
+             <p>No. 100, Ln. 00, Chifeng St., Datong Dist., Taipei City 103, Taiwan (R.O.C.)</p>
+             <p>Open  11:00 - 22:00</p>
+             
+         </div>
       </div>
+      
+     
+      
       
     </div>
   );
